8335f21c234b893d797b2d09f85672813f6e1367
Farooq Abdulla <farooqabdulla279@gmail.com>
2024-02-29 01:56:37 -0600

    updated path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "src/App.css";
-import SearchBox from "src/SearchBox.jsx";
+import "./App.css";
+import SearchBox from "./SearchBox.jsx";
 function App() {
   // const [count, setCount] = useState(0)
 
